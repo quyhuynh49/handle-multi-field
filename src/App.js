d31f563e8de3643f2996d1f0c2ec0e3e4cfb6d70
quyhuynh49 <quyhuynh49@gmail.com>
2023-11-29 00:15:51 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [list, setList] = useState([{}]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {list.length > 0 &&
+        list.map((_, index) => {
+          return (
+            <div key={index}>
+              <fieldset>
+                <legend>Item:</legend>
+                {index > 0 && (
+                  <div>
+                    <input
+                      onChange={(event) => {
+                        console.log(event.target.checked);
+                        let prevItem = {};
+                        const newList = list.map((tempItem, tempIndex) => {
+                          if (index === tempIndex + 1) {
+                            event.target.checked
+                              ? (prevItem = { ...tempItem })
+                              : (prevItem = {});
+                          }
+
+                          return index === tempIndex
+                            ? { ...prevItem }
+                            : { ...tempItem };
+                        });
+                        console.log(newList);
+                        setList(newList);
+                      }}
+                      type="checkbox"
+                      id={`inherit ${index}`}
+                    />
+                    <label htmlFor={`inherit ${index}`}>
+                      inherit info from previous entry
+                    </label>
+                  </div>
+                )}
+                <div>
+                  <label htmlFor={`name ${index}`}>name</label>
+                  <input
+                    id={`name ${index}`}
+                    value={list[index].name || ""}
+                    onChange={(event) => {
+                      const newList = list.map((tempItem, tempIndex) => {
+                        return index === tempIndex
+                          ? { ...tempItem, name: event.target.value }
+                          : { ...tempItem };
+                      });
+                      setList(newList);
+                    }}
+                    type="text"
+                    placeholder="enter name"
+                  />
+                </div>
+                <div>
+                  <label htmlFor={`address ${index}`}>address</label>
+                  <input
+                    id={`address ${index}`}
+                    value={list[index].address || ""}
+                    onChange={(event) => {
+                      const newList = list.map((tempItem, tempIndex) => {
+                        return index === tempIndex
+                          ? { ...tempItem, address: event.target.value }
+                          : { ...tempItem };
+                      });
+                      setList(newList);
+                    }}
+                    type="text"
+                    placeholder="enter address"
+                  />
+                </div>
+                <button
+                  onClick={() => {
+                    const newList = list.filter((_, i) => i !== index);
+                    setList(newList);
+                  }}
+                >
+                  delete
+                </button>
+              </fieldset>
+            </div>
+          );
+        })}
+      <button
+        onClick={() => {
+          setList([...list, {}]);
+        }}
+      >
+        create new
+      </button>
     </div>
   );
 }
-
 export default App;
